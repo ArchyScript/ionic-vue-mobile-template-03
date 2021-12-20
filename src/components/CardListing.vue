8fe5a03a34db16ed18d1af08df3f7bd96c8c03b6
ArchyScript <64386597+ArchyScript@users.noreply.github.com>
2021-12-20 19:24:17 +0100

    Initial commit

---
+++ src/components/CardListing.vue
@@ -0,0 +1,129 @@
+<template>
+	<div>
+		<ion-grid v-for="(item, index) in data" :key="index">
+			<ion-row class="first-row">
+				<div class="company">
+					{{ item.company }}
+					<br />
+					<span class="model">
+						{{ item.model }}
+					</span>
+				</div>
+				<img :src="item.image" />
+			</ion-row>
+			<ion-row class="second-row">
+				<ion-col>
+					<div class="label">Leasing Rate</div>
+					<div class="value">
+						{{ item.rate }}
+						<span class="unit">
+							/month
+						</span>
+					</div>
+				</ion-col>
+				<ion-col>
+					<div class="label">Leasing Term</div>
+					<div class="value">
+						{{ item.term }} 
+						<span class="unit">
+							months
+						</span>
+					</div>
+				</ion-col>
+				<ion-col>
+					<div class="label">New</div>
+					<div class="value">
+						{{ item.new }}
+					</div>
+				</ion-col>
+			</ion-row>
+		</ion-grid>
+	</div>
+</template>
+
+<script>
+import { IonGrid, IonRow, IonCol } from "@ionic/vue";
+
+export default {
+	name: "card-wallet",
+	components: { IonGrid, IonRow, IonCol },
+  props: {
+		data: {
+			type: Array,
+			required: true,
+			default: () => []
+		}
+	}
+}
+</script>
+
+<style lang="scss" scoped>
+.company {
+	color: #ffffff;
+	font-weight: 500;
+	margin: 0 0 -64px 0;
+	padding: 15px;
+	z-index: 9999999999999;
+}
+
+.model {
+	font-size: 13px;
+	float: left;
+}
+
+ion-row {
+	img {
+		border-radius: 20px;
+		margin: 0;
+		padding: 0;
+	}
+
+	&.first-row {
+		border-radius: 20px 20px 0 0;
+		background: #ffffff;
+		margin: 0;
+		padding: 0;
+	}
+
+	&.second-row {
+		padding: 10px;
+		background: #ffffff;
+		border-radius: 0 0 20px 20px;
+	}
+
+	ion-col {
+		.label {
+			margin-bottom: 10px;
+			font-weight: 500;
+			font-size: 13px;
+		}
+
+		.value {
+			font-weight: 500;
+			font-size: 15px;
+			color: #96a6d2;
+		}
+
+		.unit {
+			font-size: 11px;
+		}
+	}
+}
+
+.balance, .expDate, .card-number {
+	text-align: left;
+}
+
+.balance, .card-number {
+	font-size: 20px;
+}
+
+.card-number {
+	font-size: 7vw;
+}
+
+.card-icon {
+	text-align: right;
+}
+
+</style>
---
+++ src/views/Landing.vue
@@ -0,0 +1,108 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Leasing Match</ion-title>
+        <ion-buttons slot="end">
+          <ion-button>
+            <ion-icon :icon="menu"></ion-icon>
+          </ion-button>
+          <ion-button>
+            <ion-icon :icon="grid"></ion-icon>
+          </ion-button>
+        </ion-buttons>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content :fullscreen="true">
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Tab 1</ion-title>
+        </ion-toolbar>
+      </ion-header>
+    
+      <CardListing :data="cards" />
+
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButtons, IonButton } from '@ionic/vue';
+import CardListing from "../components/CardListing";
+import { grid, menu } from 'ionicons/icons';
+
+export default  {
+  name: 'Tab1',
+  components: { IonHeader, IonToolbar, IonTitle, IonContent, IonPage, IonIcon, IonButtons, IonButton, CardListing },
+  setup() {
+    return {
+      grid,
+      menu
+    }
+  },
+  data() {
+    return {
+      cards: [
+        {
+          new: "59.990",
+          rate: "481",
+          term: "36",
+          company: "Volvo",
+          model: "XC 40",
+          image: "https://www.dailymaverick.co.za/wp-content/uploads/19-213087_new_volvo_xc40_exterior-498114-e1527721345669-1600x841.jpg"
+        },
+        {
+          new: "54.980",
+          rate: "342",
+          term: "36",
+          company: "Mercedes",
+          model: "C-Class",
+          image: "https://cdn.motor1.com/images/mgl/3xR11/s3/2019-mercedes-benz-c-class-sedan.jpg"
+        },
+        {
+          new: "43.986",
+          rate: "366",
+          term: "36",
+          company: "Lexus",
+          model: "LS",
+          image: "https://images.hgmsites.net/hug/lexus-ls_100751064_h.jpg"
+        },
+      ],
+      subscriptions: [
+        {
+          name: "Themna Makwa",
+          icon: "person",
+          cost: "19.30",
+          date: "12 Aug 2020, 03:23 am",
+        },
+        {
+          name: "Tami Muthambi",
+          icon: "person",
+          cost: "6.09",
+          date: "10 Aug 2020, 13:40 pm",
+        },
+        {
+          name: "Gary Goat",
+          icon: "person",
+          cost: "19.30",
+          date: "12 Aug 2020, 03:23 am",
+        },
+        {
+          name: "Jackie Chan",
+          icon: "person",
+          cost: "6.09",
+          date: "10 Aug 2020, 13:40 pm",
+        },
+      ],
+    };
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+ion-header {
+  padding: 7px 0;
+  margin-bottom: 10px;
+  border-bottom: none;
+}
+</style>
